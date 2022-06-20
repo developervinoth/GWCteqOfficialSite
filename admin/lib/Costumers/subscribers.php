ecbb5f6bbe3e2d5f54d773d766a26fb5cb705a6d
Vinoth Kumar Raja <47133106+developervinoth@users.noreply.github.com>
2022-06-20 13:19:44 +0530

    Final Version

---
+++ admin/lib/Costumers/subscribers.php
@@ -0,0 +1,32 @@
+<?php
+class Subscribers
+{
+    /**
+     *
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     *
+     */
+    public function __destruct()
+    {
+    }
+    
+    /**
+     * Set friendly columns\' names to order tables\' entries
+     */
+    public function setOrderingValues()
+    {
+        $ordering = [
+            'id' => 'ID',
+            'email' => 'Email',
+            'created_at' => 'Date'
+        ];
+
+        return $ordering;
+    }
+}
+?>
